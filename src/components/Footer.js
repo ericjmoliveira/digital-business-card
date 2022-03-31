0d69ef9ae7f322d3d6e66a3b55872a01a0fb6e11
Eric Oliveira <ericjmoliveira@gmail.com>
2022-03-31 18:11:31 -0300

    Few updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,10 @@ import githubIcon from '../images/github-icon.png';
 function Footer() {
 	return (
 		<footer className="footer__container">
-			<button>
-				<img src={twitterIcon} alt="Twitter" />
-			</button>
-			<button>
-				<img src={facebookIcon} alt="Facebook" />
-			</button>
-			<button>
-				<img src={instagramIcon} alt="Instagram" />
-			</button>
-			<button>
-				<img src={githubIcon} alt="GitHub" />
-			</button>
+			<img src={twitterIcon} alt="Twitter" />
+			<img src={facebookIcon} alt="Facebook" />
+			<img src={instagramIcon} alt="Instagram" />
+			<img src={githubIcon} alt="GitHub" />
 		</footer>
 	);
 }
