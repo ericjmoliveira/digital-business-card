b146772824f603b71938a9213d76ba26fd42e8a1
Eric Oliveira <ericjmoliveira@gmail.com>
2022-03-31 17:08:19 -0300

    Update social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,18 @@ import githubIcon from '../images/github-icon.png';
 function Footer() {
 	return (
 		<footer className="footer__container">
-			<a href="/">
-				<img src={twitterIcon} alt="Twitter icon" />
-			</a>
-			<a href="/">
-				<img src={facebookIcon} alt="Facebook icon" />
-			</a>
-			<a href="/">
-				<img src={instagramIcon} alt="Instagram icon" />
-			</a>
-			<a href="/">
-				<img src={githubIcon} alt="GitHub icon" />
-			</a>
+			<button>
+				<img src={twitterIcon} alt="Twitter" />
+			</button>
+			<button>
+				<img src={facebookIcon} alt="Facebook" />
+			</button>
+			<button>
+				<img src={instagramIcon} alt="Instagram" />
+			</button>
+			<button>
+				<img src={githubIcon} alt="GitHub" />
+			</button>
 		</footer>
 	);
 }
